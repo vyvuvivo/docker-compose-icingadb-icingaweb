2deebf4456c2e5a0486f98890850bbd03e287a2e
vyvuvivo <vyvuvivo@gmail.com>
2023-08-11 09:08:35 +0700

    update grafana module config

--- init-icinga2.sh
+++ init-icinga2.sh
@@ -4,6 +4,9 @@ set -e
 set -o pipefail
 source ./.env
 
+# Update grafana config
+sed "s/\$VIP_DATABASE_HOST/${VIP_DATABASE_HOST:-localhost}/" ./grafana-config.ini
+
 # Create api-user
 if [ ! -f /etc/icinga2/conf.d/icingaweb-api-user.conf ]; then
   sed "s/\$ICINGAWEB_ICINGA2_API_USER_PASSWORD/${ICINGAWEB_ICINGA2_API_USER_PASSWORD:-icingaweb}/" ./icingaweb-api-user.conf > /etc/icinga2/conf.d/icingaweb-api-user.conf
